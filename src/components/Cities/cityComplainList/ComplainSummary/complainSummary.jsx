08ed7241d878f9773567b3acc2b151cc671cec99
Ahmiii <ahmii.shaikh18@gmail.com>
2020-09-14 18:38:47 +0500

    Update status functionlity to admin

--- src/components/Cities/cityComplainList/ComplainSummary/complainSummary.jsx
+++ src/components/Cities/cityComplainList/ComplainSummary/complainSummary.jsx
@@ -1,58 +1,78 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-const CompalinSummary = (props) => {
-  const [updatedStatus, setupdatedStatus] = useState(props.compalinlist.status);
-  const id = props.compalinlist.id;
-
-  const onStatusUpdate = (e) => {
-    e.preventDefault();
-    setupdatedStatus(e.target.value);
-    console.log(id);
+class CompalinSummary extends Component {
+  state = {
+    id: "",
+    status: "",
   };
-  return (
-    <div className="container section project-detail">
-      <div className="row">
-        <div className="s12 m4 l8">
-          <div className="card z-depth-0">
-            <div className="card-content">
-              <p>{props.compalinlist.content}</p>
-            </div>
 
-            <div className="card-action grey lighten-4 grey-text">
-              <p>
-                Posted by{" "}
-                {props.compalinlist.userFirstName[0].toUpperCase() +
-                  props.compalinlist.userFirstName.substring(1)}{" "}
-                {props.compalinlist.userLastName[0].toUpperCase() +
-                  props.compalinlist.userLastName.substring(1)}
-              </p>
-              <p>
-                {new Date(
-                  props.compalinlist.createdAt.seconds * 1000
-                ).toLocaleDateString("en-US")}
-              </p>
+  onStatusUpdate = (e) => {
+    e.preventDefault();
+    this.setState({ status: e.target.value });
+    this.setState({ id: this.props.compalinlist.id });
+  };
+
+  onHandleSubmit = () => {
+    this.props.upDateComplainStatus(this.state);
+  };
+
+  render() {
+    return (
+      <div className="container section project-detail">
+        <div className="row">
+          <div className="s12 m4 l8">
+            <div className="card z-depth-0">
+              <div className="card-content">
+                <p>{this.props.compalinlist.content}</p>
+              </div>
+
+              <div className="card-action grey lighten-4 grey-text">
+                <p>
+                  Posted by{" "}
+                  {this.props.compalinlist.userFirstName[0].toUpperCase() +
+                    this.props.compalinlist.userFirstName.substring(1)}{" "}
+                  {this.props.compalinlist.userLastName[0].toUpperCase() +
+                    this.props.compalinlist.userLastName.substring(1)}
+                </p>
+                <p>
+                  {new Date(
+                    this.props.compalinlist.createdAt.seconds * 1000
+                  ).toLocaleDateString("en-US")}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="s12 m4 l2">
-          <div className="card z-depth-0">
-            {/* <div className="card-content"> */}
-            <label>Applicatin Status</label>
-            {props.userCredential.userType === "admin" ? (
-              <select onChange={onStatusUpdate} style={{ display: "block" }}>
-                <option value="Issue Resolved">Issue Resolved</option>
-                <option value="In Progress">In Progres</option>
-              </select>
-            ) : (
-              <p>{props.compalinlist.status}</p>
-            )}
+          <div className="s12 m4 l2">
+            <div className="card z-depth-0">
+              {/* <div className="card-content"> */}
+              <label>Application Status</label>
+              {this.props.userCredential.userType === "admin" ? (
+                <>
+                  <select
+                    onChange={this.onStatusUpdate}
+                    style={{ display: "block" }}
+                  >
+                    <option value="Issue Resolved">Issue Resolved</option>
+                    <option value="In Progress">In Progres</option>
+                  </select>
+                  <button onClick={this.onHandleSubmit}>Submit</button>
+                </>
+              ) : (
+                <p>{this.props.compalinlist.status}</p>
+              )}
 
-            {/* </div> */}
+              {/* </div> */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    upDateComplainStatus: (updatedStatus) => dispatch(upDateComplainStatus()),
+  };
 };
-
-export default CompalinSummary;
+export default connect(null, mapDispatchToProps)(CompalinSummary);
